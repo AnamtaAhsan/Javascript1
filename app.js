85e646165c471b67644aed2b773c4d28d472b0f9
AnamtaAhsan <anamtaahsan3@gmail.com>
2025-05-22 09:01:00 -0700

    2 change

--- app.js
+++ app.js
@@ -1,7 +1,224 @@
-alert("Hii")
-document.write("Hello World")
- 
-var a = 5;
-var b = 6;
-var c = a+b;
-alert(c)
+// document.write("Hello World");
+
+// CHAPTER:1 (Alert)
+// alert("Welcome to our website!");
+
+// alert("Error! Please enter a valid password.");
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding!");
+
+// console.log("Hello...I can run JS through my web browser's console");
+// alert("Hello...I can run JS through my web browser's console");
+
+// CHAPTER:2 (Variables For Strings)
+// var username;
+
+// var myName = "Anamta Ahsan"; 
+
+// var message;
+// message = "Hello World";
+// // alert(message);
+
+// var name = "Jhone Doe"
+// alert(name)
+// var age = "15 years old"
+// alert(age)
+// var certified = "Certified Mobile Application Development"
+// alert(certified)
+
+// alert("PIZZA\n");
+
+// var email = "My email address is example"
+// var address = "@example.com"
+// var emailaddress = email+address
+// alert(emailaddress)
+
+// var book = "A smarter way to learn JS"
+// alert(book)
+
+// document.write("Yah! I can write HTML content through Javascript")
+
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(a)
+
+// CHAPTER:3 (Varables For Numbers)
+
+// var age = ("I am 19 years old") 
+// alert(age)
+
+// var a;  
+// here we declare variable
+// a = "You have visited this site 7 times";
+// and initialize here 
+// var a ="You have visited this site 8 times"; 
+// alert(a);
+
+// var birthYear = 2005
+// document.write("My birth year is 2005")
+
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt(s)";
+// var quantity = 5;
+// var message = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store”."
+// document.write(message);
+
+
+// var Anamta, Ahsan, 19;
+// Legal
+// var Anamta, _Ahsan, $13, 27, khilji;
+// Illegal
+// var 1anamta, Anamta-ahsan, var, alert, @play;
+
+// document.write("<h1>Rules for naming JS variables</h1><br><br>");
+// document.write("Variable names can only contain letters, numbers, $, and _.For example:$my_1stVariable <br>");
+// document.write("Variables must begin with a letter, $ or _. For example:$name, _name, or name<br>");
+// document.write("Variable names are case sensitive.<br>");
+// document.write("Variable names should not be JS keywords.");
+
+// CHAPTER:5 (Math Expressions)
+// var a = 27;
+// var b = 3;
+// var c= a+b;
+// alert(c)
+
+// document.write("The sum of 27 and 3 is 30");
+
+// var a = 27;
+// var b = 3;
+// var c= a-b;
+// alert(c)
+
+// document.write("The difference between 27 and 3 is 24");
+
+// var a = 27;
+// var b = 3;
+// var c= a*b;
+// alert(c)
+
+// document.write("27 times 3 is 81 or 3 times 27 is 81");
+
+// var a = 27;
+// var b = 3;
+// var c= a/b;
+// alert(c)
+
+// document.write("27 divided by 3 is 9");
+
+// Declare a variable
+// var a;
+// // value of variable after declaration
+// document.write("Value after variable declaration is undefined <br>");
+// // Initialize the variable
+// a = 8;
+// // initial value
+// document.write("Initial value: 8 <br>");
+// // Increment
+// a++;
+// // value after increment
+// document.write("Value after increment is: 9 <br>");
+// // Add 7 to  variable
+// a = a++ + 7;
+// // value after addition
+// document.write("Value after addition is:16 <br>");
+// // Decrement
+// a--;
+// // value after decrement
+// document.write("Value after decrement is: 15<br>");
+// // remainder after dividing the variable's value by 3
+// var remainder = a % 3;
+// // value of remainder
+// document.write("The remainder is: 0");
+
+
+// var Price = 600;
+// var Tickets = 5;
+// var cost = Price * Tickets;
+// document.write("Total cost to buy 5 tickets to a movie is 3000PKR");
+
+// var number = 7;
+// document.write("<h2>Multiplication Table of 7 </h2>");
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(number + " x " + i + " = " + (number * i) + "<br>");
+// }
+
+// var celsiusTemp = 25;
+// // Convert to Fahrenheit
+// var fahrenheitFromCelsius = (celsiusTemp * 9 / 5) + 32;
+// document.write("25°C is 77°F <br>");
+// // Fahrenheit temperature into a variable
+// var fahrenheitTemp = 70;
+// // Convert to Celsius
+// var celsiusFromFahrenheit = (fahrenheitTemp - 32) * 5 / 9;
+// document.write("70°F is 21.11111111111111°C");
+
+// var Item1 = 650;
+// var Item2 = 100;
+// var quantityItem1 = 3;
+// var quantityItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (Item1 * quantityItem1) + (Item2 * quantityItem2) + shippingCharges;
+
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("Price of item 1 is 650 <br>");
+// document.write("Quantity of item 1 is 3 <br>");
+// document.write("Price of item 2 is 100 <br>");
+// document.write("Quantity of item 2 is 7 <br>");
+// document.write("Shipping Charges 100 <br><br>");
+// document.write("Total cost of your order is 2750 PKR");
+
+
+// var totalMarks = 500;
+// var marksObtained = 420;
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("Total Marks: 500 <br>");
+// document.write("Marks Obtained: 420 <br>");
+// document.write("Percentage: 84%");
+
+
+// var totalPKR = (10 * 104.80) + (25 * 28);
+// document.write("<h1>Currency in PKR</h1>");
+// document.write("Total Currency in PKR: 1740");
+
+// var a = 10;
+// var a = 5*10/2;
+
+// var currentyear = 2025;
+// var birthyear = 2003;
+// var age1 = currentyear-birthyear;
+// var age2 = age1-1;
+// document.write("<h1>Age Calculator</h1><br><br>");
+// document.write("Current Year: 2025<br>");
+// document.write("Birth Year: 2003<br>");
+// document.write("They are either 22 or 21 years old ");
+
+// var radius = 5; 
+// var pi = 3.142;
+// var circumference = 2 * pi * radius;
+// var area = pi * (radius * radius);
+// document.write("<h1>The Geometrizer </h1><br>");
+// document.write("Radius of a circle: 5 <br>");
+// document.write("The circumference is: 31.419999999999998 <br>");
+// document.write("The area is: 78.55 <br>");
+
+
+var favoriteSnack = "chocolate cookies";
+var currentage = 19;
+var maxage = 90;
+var amountperday = 5;
+var yearsleft = maxage - currentage;
+var totalneeded = yearsleft * amountperday;
+document.write("<h1>Lifetime Supply Calculator </h1> <br><br>")
+document.write("Favourite Snack : Choclate cookies <br>");
+document.write("Current age: 19 <br>");
+document.write("Estimated Maxage : 90 <br>");
+document.write("Amount of Snack perday: 5  <br>");
+document.write("You will need 355 chocolate cookies to last you until the ripe old age of 90.");
+
